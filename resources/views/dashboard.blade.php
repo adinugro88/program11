76a9ce7d86558375806dc5e7e86432fb5e151425
adinugro88 <adin5337@gmail.com>
2024-11-06 19:43:03 +0700

    update all

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,151 @@
+<x-app-layout>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-xl-4 row-cols-xxl-4">
+        <div class="col">
+          <div class="card radius-10 border-0 border-start border-primary border-4">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="">
+                  <p class="mb-1">Total Orders</p>
+                  <h4 class="mb-0 text-primary">248</h4>
+                </div>
+                <div class="ms-auto widget-icon bg-primary text-white">
+                  <i class="bi bi-basket2-fill"></i>
+                </div>
+              </div>
+              <div class="progress mt-3" style="height: 4.5px;">
+                <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+         </div>
+         <div class="col">
+          <div class="card radius-10 border-0 border-start border-success border-4">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="">
+                  <p class="mb-1">Total Revenue</p>
+                  <h4 class="mb-0 text-success">$1,245</h4>
+                </div>
+                <div class="ms-auto widget-icon bg-success text-white">
+                  <i class="bi bi-currency-dollar"></i>
+                </div>
+              </div>
+              <div class="progress mt-3" style="height: 4.5px;">
+                <div class="progress-bar bg-success" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+         </div>
+         <div class="col">
+          <div class="card radius-10 border-0 border-start border-danger border-4">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="">
+                  <p class="mb-1">Bounce Rate</p>
+                  <h4 class="mb-0 text-danger">24.25%</h4>
+                </div>
+                <div class="ms-auto widget-icon bg-danger text-white">
+                  <i class="bi bi-graph-down-arrow"></i>
+                </div>
+              </div>
+              <div class="progress mt-3" style="height: 4.5px;">
+                <div class="progress-bar bg-danger" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+         </div>
+         <div class="col">
+          <div class="card radius-10 border-0 border-start border-warning border-4">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="">
+                  <p class="mb-1">New Users</p>
+                  <h4 class="mb-0 text-warning">214</h4>
+                </div>
+                <div class="ms-auto widget-icon bg-warning text-dark">
+                  <i class="bi bi-people-fill"></i>
+                </div>
+              </div>
+              <div class="progress mt-3" style="height: 4.5px;">
+                <div class="progress-bar bg-warning" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+         </div>
+      </div>
+      <!--end row-->
+
+
+       <div class="row">
+          <div class="col-lg-8">
+            <div class="card">
+              <div class="card-header bg-transparent">
+                <div class="d-flex align-items-center">
+                  <div class="">
+                    <h6 class="mb-0 fw-bold">Sales Overview</h6>
+                  </div>
+                  <div class="dropdown ms-auto">
+                    <button type="button" class="btn-option dropdown-toggle dropdown-toggle-nocaret cursor-pointer" data-bs-toggle="dropdown"><i class="bi bi-three-dots fs-4"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="javascript:;">Action</a>
+                      </li>
+                      <li><a class="dropdown-item" href="javascript:;">Another action</a>
+                      </li>
+                      <li>
+                        <hr class="dropdown-divider">
+                      </li>
+                      <li><a class="dropdown-item" href="javascript:;">Something else here</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                   <div id="chart1"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-header bg-transparent">
+                <div class="d-flex align-items-center">
+                  <div class="">
+                    <h6 class="mb-0 fw-bold">Statistic</h6>
+                  </div>
+                  <div class="dropdown ms-auto">
+                    <button type="button" class="btn-option dropdown-toggle dropdown-toggle-nocaret cursor-pointer" data-bs-toggle="dropdown"><i class="bi bi-three-dots fs-4"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="javascript:;">Action</a>
+                      </li>
+                      <li><a class="dropdown-item" href="javascript:;">Another action</a>
+                      </li>
+                      <li>
+                        <hr class="dropdown-divider">
+                      </li>
+                      <li><a class="dropdown-item" href="javascript:;">Something else here</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                   <div id="chart2"></div>
+              </div>
+              <ul class="list-group list-group-flush mb-0">
+                <li class="list-group-item border-top d-flex justify-content-between align-items-center bg-transparent">Mobile<span class="badge bg-success rounded-pill">25%</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center bg-transparent">Desktop<span class="badge bg-primary rounded-pill">65%</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center bg-transparent">Tablet<span class="badge bg-danger rounded-pill">10%</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+       </div><!--end row-->
+
+
+
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="dark">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+
+        <!-- Plugins CSS -->
+        <link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet">
+
+        <!-- Loader -->
+        <link href="{{ asset('assets/css/pace.min.css') }}" rel="stylesheet">
+        <script src="{{ asset('assets/js/pace.min.js') }}"></script>
+
+        <!-- Main Styles -->
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
+        <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/dark-theme.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/semi-dark-theme.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/minimal-theme.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/shadow-theme.css') }}" rel="stylesheet">
+
+        <!-- Livewire Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+
+        @include('layouts.header');
+
+        @include('layouts.aside');
+
+            <!-- Page Content -->
+            <main class="page-content">
+                {{ $slot }}
+            </main>
+
+        <!--start overlay-->
+        <div class="overlay btn-toggle-menu"></div>
+        <!--end overlay-->
+        @stack('modals')
+
+        <!-- Livewire Scripts -->
+        @livewireScripts
+
+        <!-- Plugins JavaScript -->
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+        <script src="{{ asset('assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+        <script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+        <script src="{{ asset('assets/plugins/apex/apexcharts.min.js') }}"></script>
+        <script src="{{ asset('assets/js/index.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/js/main.js') }}"></script>
+    </body>
+</html>
