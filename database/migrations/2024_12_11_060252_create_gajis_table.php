cfbc57d50c5de91db9bdeb445f28047fb8baca9f
adinugro88 <adin5337@gmail.com>
2024-12-11 14:30:34 +0700

    update main

---
+++ database/migrations/2024_12_11_060252_create_gajis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gajis', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_kode');
+            $table->date('tanggal');
+            $table->BigInteger('karyawan_id')->unsigned();
+            $table->foreign('karyawan_id')->references('id')->on('karyawans')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gajis');
+    }
+};
